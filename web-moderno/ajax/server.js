85dbb8aca73b5be23aee18792e40ca905f724456
Leonardo Montero <leonardo.tmontero@telefonica.com>
2021-09-18 09:15:21 -0300

    feat: XMLHttpRequest

---
+++ web-moderno/ajax/server.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+// middlewares
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json)
+
+app.get('/teste', (req, res) => { res.send('OK') })
+
+app.listen(8080, () => console.log('Excutando...'))
