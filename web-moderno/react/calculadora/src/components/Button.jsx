b5474a361f73acdd28a1a00272dc34d1b21ff4d5
Leonardo Montero <leonardo.tmontero@telefonica.com>
2021-09-22 17:46:36 -0300

    feat: finish calculator

--- web-moderno/react/calculadora/src/components/Button.jsx
+++ web-moderno/react/calculadora/src/components/Button.jsx
@@ -8,10 +8,10 @@ export default props => {
     classes += props.triple ? 'triple' : ''
 
     return (
-        <button
-            onClick={() => props.click && props.click(props.label)}
+        <button 
+            onClick={e => props.click && props.click(props.label)}
             className={classes}>
-                {props.label}
+            {props.label}
         </button>
     )
 }
